164b430d4390a2da74b7225e95845c5b61e57936
beetlerat <beetlerat@yandex.ru>
2025-05-01 14:53:37 +0300

    ME-36 Создание сцены для разработки UI контента для планшета
    Dev:
    
    - Созданы сцены `CreateTabletRoomUITemplate` и `CreateTabletRoomUI`. `CreateTabletRoomUITemplate` это шаблон сцены, что бы после очередного разработанного UI не пришлось очищать созданный UI. А `CreateTabletRoomUI` это сцена, в которой производится разработка UI
    - Необходимые скрипты вынесены в SDK

---
+++ Assets/ApplicationContent/APIScripts/External/Tablet/UI/UITabletRoomContentSupplier.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/// <summary>
+/// <para>Компонент передающий планшету контент текущей комнаты.</para>
+/// </summary>
+public sealed class UITabletRoomContentSupplier : MonoBehaviour
+{
+    [SerializeField] private GameObject _content;
+
+    /// <summary>
+    /// <para>Получить контент текущей комнаты.</para>
+    /// </summary>
+    /// <returns>контент текущей комнаты</returns>
+    public GameObject GetRoomContent()
+    {
+        return _content;
+    }
+}
